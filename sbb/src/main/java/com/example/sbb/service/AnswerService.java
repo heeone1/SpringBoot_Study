e0cfc6e779102df8f6fc1f34f5b6c3abf3a6a302
heeone1 <rinakim1019@gmail.com>
2024-07-23 04:45:59 +0900

    수정,삭제 기능

--- sbb/src/main/java/com/example/sbb/service/AnswerService.java
+++ sbb/src/main/java/com/example/sbb/service/AnswerService.java
@@ -1,5 +1,6 @@
 package com.example.sbb.service;
 
+import com.example.sbb.DataNotFoundException;
 import com.example.sbb.entity.Answer;
 import com.example.sbb.entity.Question;
 import com.example.sbb.entity.SiteUser;
@@ -8,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,5 +25,24 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id) {
+        Optional<Answer> answer = this.answerRepository.findById(id);
+        if (answer.isPresent()) {
+            return answer.get();
+        } else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content) {
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer) {
+        this.answerRepository.delete(answer);
     }
 }
